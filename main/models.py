3b8e45cca50251e2f02e75eb8336a5a5dee6f05c
miguelCapeleiro <miguelcapeleiro2025@gmail.com>
2025-12-02 08:38:19 -0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -20,27 +20,3 @@ class Usuario(models.Model):
     class Meta:
         ordering = ['nome']
 
-    def __str__(self):
-        return self.nome
-
-
-class Materia(models.Model):
-    nome = models.CharField(max_length=50)
-
-    class Meta:
-        ordering = ['nome']
-
-    def __str__(self):
-        return self.nome
-
-class Assunto(models.Model):
-    titulo = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=500)
-    usuario_criador_id = models.ForeignKey(Usuario, on_delete=models.CASCADE,)
-    materia_id = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='assunto')
-
-    class Meta:
-        ordering = ['materia_id', 'titulo']
-
-    def __str__(self):
-        return self.titulo
