f38289570f34887bceb461731c2e07cfe3e9615f
miguelCapeleiro <miguelcapeleiro2025@gmail.com>
2025-12-02 11:34:51 -0300

    parte do projeto

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+
+    return render(request, 'site/home.html')
+
+def perfil_aluno(request):
+    return render(request, 'site/perfil_aluno.html')
+
+def perfil_professores(request):
+    return render(request, 'site/perfil_professores.html')
